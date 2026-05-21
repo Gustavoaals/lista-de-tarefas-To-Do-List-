370809c5c90a3d428620141b16d8be3627947d9d
Gustavo Alves <gustavoalvess842@gmail.com>
2026-05-21 10:57:04 -0300

    add correção

--- to-do-list.py
+++ to-do-list.py
@@ -1,9 +1,17 @@
-from flask import flask, request
+from flask import Flask, request
 
-app = flask(__name__)
+app = Flask(__name__)
 
 tarefas = []
 
 @app.route("/tareas", methods =["GET"])
 def listar():
     return {"tarefas": tarefas}
+
+@app.route("/tarefas", methods =["POST"])
+def adicionar():
+    dados = request.json
+    tarefas.append(dados["tarefas"])
+    return{"mensagem": "Tarefa adicionada" }
+
+app.run(debug=True)
